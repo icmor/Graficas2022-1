0d340c1f27ee1e5899323b1ea0806162e4c6c803
icmor <cornejodlm@ciencias.unam.mx>
2022-03-18 11:14:51 -0600

    Formato
    Todo editor/IDE de python va a insertar cuatro espacios al presionar tab.
    https://peps.python.org/pep-0008/#tabs-or-spaces
    
    Use el siguiente comando:
    for file in "$(ls)"; do expand -t 4 "$file" | tee "$file"; done

--- pruebas.py
+++ pruebas.py
@@ -4,35 +4,27 @@ from grafica import grafica
 
 #Main del programa
 if __name__ == '__main__':
-	
-	#vertices
-	v1 = vertice("v1")
-	v2 = vertice("v2")
-	v3 = vertice("v3")
-	v4 = vertice("v4")
 
-	#aristas
-	a1 = arista(v1, v2)
-	a2 = arista(v2, v3)
-	a3 = arista(v3, v4)
-	a4 = arista(v4, v1)
+    #vertices
+    v1 = vertice("v1")
+    v2 = vertice("v2")
+    v3 = vertice("v3")
+    v4 = vertice("v4")
 
-	#conjuntos de vertices
-	vertices = [v1, v2, v3, v4]
+    #aristas
+    a1 = arista(v1, v2)
+    a2 = arista(v2, v3)
+    a3 = arista(v3, v4)
+    a4 = arista(v4, v1)
 
-	#conjunto de aristas
-	aristas = [a1, a2, a3, a4]
+    #conjuntos de vertices
+    vertices = [v1, v2, v3, v4]
 
-	#Grafica
-	g = grafica(vertices, aristas)
-	
-	#imprimimos
-	print(str(g))
+    #conjunto de aristas
+    aristas = [a1, a2, a3, a4]
 
+    #Grafica
+    g = grafica(vertices, aristas)
 
-
-
-
-
-
-
+    #imprimimos
+    print(str(g))
